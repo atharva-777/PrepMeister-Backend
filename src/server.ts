ed8ca112d480e7d4c1fe309927f87b709bb49593
Atharva Jadhav <jadhavatharva499@gmail.com>
2023-10-29 23:39:48 +0530

    created instance of server

--- src/server.ts
+++ src/server.ts
@@ -1,2 +1,8 @@
 import express from "express";
-import app from "../app";
+import app from "./app";
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT,()=>{
+    console.log(`Listening on http://localhost:${PORT}`);
+})
